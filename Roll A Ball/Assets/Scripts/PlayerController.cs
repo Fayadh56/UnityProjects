74df47536a2164c3ccde4b46aa92e645775bb2ce
Fayadh A <fayadh56@gmail.com>
2017-01-28 11:57:21 -0500

    Finished Roll A Ball

--- Roll A Ball/Assets/Scripts/PlayerController.cs
+++ Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
-
+	public Text countText;
+	public Text winText;
 	private Rigidbody rb;
 	public float speed;
+	private int score;
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
+		score = 0;
+		setScoreText();
+		winText.text="";
 	}
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis("Horizontal");
@@ -16,5 +22,20 @@ public class PlayerController : MonoBehaviour {
 		// use this input to add forces, and interact with the rigid body
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce(movement*speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Crate")) {
+			other.gameObject.SetActive(false);
+			score++;
+			setScoreText();
+		}
+	}
+
+	void setScoreText() {
+		countText.text = "Score: " + score.ToString();
+		if (score >= 15) {
+		winText.text="Congratulations You've Won!";
+		}
 	}
 }
